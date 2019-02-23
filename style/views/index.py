f0f4895ddc0a804bd84cc6a1ed872f63dfc2ffc9
xrhan <xrhan@umich.edu>
2019-02-22 21:11:44 -0500

    hello world

--- style/views/index.py
+++ style/views/index.py
@@ -6,9 +6,9 @@ import os
 import flask
 import style
 
-@style.app.route('/', methods=["GET", "POST"])
+@style.app.route('/', methods=["GET"])
 def show_index():
     """Display / route."""
-    if 'username' not in flask.session:
-        return flask.redirect('/accounts/login/')
-    return flask.render_template("index.html", **context)
+    # if 'username' not in flask.session:
+    #     return flask.redirect('/accounts/login/')
+    return json.dumps({'data':'Hello World'})
