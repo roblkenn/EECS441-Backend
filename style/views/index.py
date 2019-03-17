a735b8d8ea3193d682a22d62fa1000f81b70734c
xrhan <xrhan@umich.edu>
2019-03-17 19:31:24 -0400

    password

--- style/views/index.py
+++ style/views/index.py
@@ -21,6 +21,42 @@ def send_file(filename):
         filename, as_attachment=True
     )
 
-@style.app.route()
+@style.app.route('/signup', methods=['GET', 'POST'])
 def sign_up():
 
+if flask.request.method == "POST":
+
+        # get user name and password from form
+        # password = 
+        # fullname = 
+        # file = 
+
+        # otherwise salt and hash inputted password
+        salt = uuid.uuid4().hex
+        hash_obj = hashlib.new('sha512')
+        password_salted = salt + password
+        hash_obj.update(password_salted.encode('utf-8'))
+
+        # Save POST request's file object to a temp file
+        dummy, temp_filename = tempfile.mkstemp()
+        file.save(temp_filename)
+        # Compute filename
+        hash_txt = sha256sum(temp_filename)
+        dummy, suffix = os.path.splitext(file.filename)
+        hash_filename_basename = hash_txt + suffix
+        hash_filename = os.path.join(
+            style.app.config["UPLOAD_FOLDER"],
+            hash_filename_basename
+        )
+
+        # Move temp file to permanent location
+        shutil.move(temp_filename, hash_filename)
+        style.app.logger.debug("Saved %s", hash_filename_basename)
+
+        # update database
+
+        flask.session["username"] = username
+        return flask.redirect('/')
+
+    else:
+        return flask.redirect('/')
