a63bb9f666955587d80cf0ce94b65bed68018b48
Robert Kennedy <robert076kennedy@gmail.com>
2019-03-22 11:04:33 -0400

    - Added sell api endpoint to application.py

--- application.py
+++ application.py
@@ -19,5 +19,8 @@ app.register_blueprint(auth.bp)
 import dataset
 app.register_blueprint(dataset.bp)
 
+import sell
+app.register_blueprint(sell.bp)
+
 if __name__ == "__main__":
 	app.run(host='0.0.0.0')
