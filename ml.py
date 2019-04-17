c99cbce2477f08f70560955d4b4b6ca62941dbe0
Robert Kennedy <robert076kennedy@gmail.com>
2019-04-16 20:57:16 -0400

    - Updated the ml.py script it's all working great now.

--- ml.py
+++ ml.py
@@ -1,18 +1,18 @@
 from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.layers import Dense, Input, Conv2D, Flatten
 from tensorflow.keras.wrappers.scikit_learn import KerasRegressor
-from tensorflow.keras.image_preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from sklearn.model_selection import cross_val_score, KFold, train_test_split
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.pipeline import Pipeline
 from sklearn.datasets import make_regression
 from json import JSONDecoder
 
+import tensorflowjs as tfjs
 import matplotlib.pyplot as plt
 import pandas as pd
 import cv2
 import numpy as np
-import tensorflowjs as tfjs
 
 EPOCHS = 75
 INIT_LR = 1e-3
@@ -35,67 +35,58 @@ def train_model(X_train, y_train, X_test, y_test):
     model.add(Conv2D(64, kernel_size=3, activation='relu', input_shape=IMAGE_DIMS))
     model.add(Conv2D(32, kernel_size=3, activation='relu'))
     model.add(Flatten())
-    model.add(Dense(5, activation='linear'))
+    model.add(Dense(4, activation='linear'))
 
     model.summary()
 
     # optimizer = "rmsprop" 
-    model.compile(optimizer = "adam", loss = 'mse', metrics=['accuracy'])
+    model.compile(optimizer = "adam", loss = 'mse', metrics=['mean_absolute_error', 'mean_squared_error'])
 
     # , validation_split=0.2
-    history = model.fit_generator(datagen.flow(X_train, y_train, batch_size=32), steps_per_epoch=len(X_train) / 32, epochs=10, verbose=1)
+    history = model.fit_generator(datagen.flow(X_train, y_train, batch_size=32), validation_data=datagen.flow(X_test, y_test), epochs=16, verbose=1)
     # history = model.fit(X_train, y_train, epochs=10, verbose=1)
 
-    model.evaluate(X_test, y_test)
+    # model.evaluate(X_test, y_test)
 
     return model, history
 
 def plot_history(history):
-    hist = pd.DataFrame(history.history)
-    hist['epoch'] = history.epoch
+	hist = pd.DataFrame(history.history)
+	hist['epoch'] = history.epoch
 
-    plt.figure()
-    plt.xlabel('Epoch')
-    plt.ylabel('Mean Abs Error [MPG]')
-    plt.plot(hist['epoch'], hist['mean_absolute_error'],
-            label='Train Error')
-    plt.plot(hist['epoch'], hist['val_mean_absolute_error'],
-            label = 'Val Error')
-    plt.ylim([0,5])
-    plt.legend()
+	plt.figure()
+	plt.xlabel('Epoch')
+	plt.ylabel('Mean Abs Error [MPG]')
+	plt.plot(hist['epoch'], hist['mean_absolute_error'],
+			label='Train Error')
+	plt.plot(hist['epoch'], hist['val_mean_absolute_error'],
+			label = 'Val Error')
+	plt.ylim([0,5])
+	plt.legend()
 
-    plt.figure()
-    plt.xlabel('Epoch')
-    plt.ylabel('Mean Square Error [$MPG^2$]')
-    plt.plot(hist['epoch'], hist['mean_squared_error'],
-            label='Train Error')
-    plt.plot(hist['epoch'], hist['val_mean_squared_error'],
-            label = 'Val Error')
-    plt.ylim([0,20])
-    plt.legend()
-    plt.show()
-
-    plot_history(history)
+	plt.figure()
+	plt.xlabel('Epoch')
+	plt.ylabel('Mean Square Error [$MPG^2$]')
+	plt.plot(hist['epoch'], hist['mean_squared_error'],
+			label='Train Error')
+	plt.plot(hist['epoch'], hist['val_mean_squared_error'],
+			label = 'Val Error')
+	plt.ylim([0,20])
+	plt.legend()
+	plt.show()
 
 
 def save_model(model):
-    # serialize model to JSON
-#     model_json = model.to_json()
-#     with open("model.json", "w") as json_file:
-#         json_file.write(model_json)
-    # serialize weights to HDF5
-    #     model.save("model.h5")
-
     # Serialize to tensorflowjs model
-    tfjs.converters.save_keras_model(model, '')
+    tfjs.converters.save_keras_model(model, 'export')
 
 def load_dataset():
     with open('./dataset/dataset.json', 'r') as file:
         dataset = JSONDecoder().decode(file.read())
-
-    X = [cv2.resize(cv2.imread('./dataset/' + item.blobName + '.jpg')) for item in dataset]
+	
+    X = [cv2.resize(cv2.imread('./dataset/' + item['blobName'] + '.jpg', 1), (100, 100)) for item in dataset]
     X = np.array(X)
-    y = [[item.contrast, item.brightness, item.saturation, item.temperature] for item in dataset]
+    y = [[item['contrast'], item['brightness'], item['saturation'], item['temperature']] for item in dataset]
     y = np.array(y)
 
     print(X.shape, y.shape)
